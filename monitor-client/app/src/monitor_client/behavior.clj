6cc38124525743bd213c52e4d56d0f9be9c05836
konrad-garus <konrad.garus@gmail.com>
2013-10-24 10:57:32 +0200

    Derive [:received :count-history]

--- monitor-client/app/src/monitor_client/behavior.clj
+++ monitor-client/app/src/monitor_client/behavior.clj
@@ -3,10 +3,19 @@
               [io.pedestal.app.messages :as msg]
               [io.pedestal.app :as app]))
 
+(def history-entries 5)
+
 (defn set-received-count [old message]
   (let [{:keys [tstamp value]} message]
     [tstamp value]))
 
+(defn derive-received-count-history [old val]
+  (let [history (concat old [val])]
+    (take-last history-entries history)))
+
 (def monitor-app
   {:version 2
-   :transform [[:set-value [:received :count] set-received-count]]})
+   :transform [[:set-value [:received :count] set-received-count]]
+   :derive #{
+             [#{[:received :count]} [:received :count-history] derive-received-count-history :single-val]}
+   })
