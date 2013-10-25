2969a7aa2220e2ddfd49a8d34dd64be025c3b61b
konrad-garus <konrad.garus@gmail.com>
2013-10-25 21:33:46 +0200

    Derive and draw received tps rather than static counter.

--- monitor-client/app/src/monitor_client/behavior.clj
+++ monitor-client/app/src/monitor_client/behavior.clj
@@ -15,15 +15,21 @@
 (defn toggle-simulation-running [old-value msg]
   (not old-value))
 
-(defn derive-received-count-history [old val]
-  (let [history (concat old [val])]
-    (take-last history-entries history)))
+(defn derive-received-tps [_ {:keys [old-model new-model]}]
+  (if-let [[_ old-count] (get-in old-model [:received :count])]
+    (let [[new-time new-count] (get-in new-model [:received :count])]
+      [new-time (- new-count old-count)])))
 
-(defn init-recd-count-history []
-  [[:node-create [:received-counts] :map]])
+(defn derive-history [old val]
+  (when val
+    (let [history (concat old [val])]
+      (take-last history-entries history))))
 
-(defn emit-recd-count-history [{:keys [old-model new-model]}]
-  [[:value [:received-counts] (get-in new-model [:received :count-history])]])
+(defn init-recd-tps-history []
+  [[:node-create [:received-tps] :map]])
+
+(defn emit-recd-tps-history [{:keys [old-model new-model]}]
+  [[:value [:received-tps] (get-in new-model [:received :tps-history])]])
 
 (defn init-connected [arg]
   [[:transform-enable [:connected] :start
@@ -42,10 +48,12 @@
    :transform [[:set-value [:received :count] set-received-count]
                [:start [:connected] connect]]
    :derive #{
-             [#{[:received :count]} [:received :count-history] derive-received-count-history :single-val]}
-   :emit [{:in #{[:received :count-history]}
-           :fn emit-recd-count-history
-           :init init-recd-count-history}
+             [#{[:received :count]} [:received :tps] derive-received-tps]
+             [#{[:received :tps]} [:received :tps-history] derive-history :single-val]
+             [#{[:received :count]} [:received :count-history] derive-history :single-val]}
+   :emit [{:in #{[:received :tps-history]}
+           :fn emit-recd-tps-history
+           :init init-recd-tps-history}
           
           {:in #{[:connected]}
            :fn emit-connected
--- monitor-client/app/src/monitor_client/rendering.cljs
+++ monitor-client/app/src/monitor_client/rendering.cljs
@@ -7,32 +7,32 @@
             [jayq.core :refer [$]])
   (:require-macros [monitor-client.html-templates :as html-templates]))
 
-(def received-counts-chart (atom nil))
+(def received-tps-chart (atom nil))
 
-(defn prepare-received-counts-chart [_ _ _]
-  (reset! received-counts-chart ($/plot 
+(defn prepare-received-tps-chart [_ _ _]
+  (reset! received-tps-chart ($/plot 
                  "#received_counts" 
                  (clj->js [])
                  (clj->js {:xaxis { :mode "time"}}))))
 
-(defn render-received-counts-chart [_ [_ _ _ new-value] _]
+(defn render-received-tps-chart [_ [_ _ _ new-value] _]
   (when (not-empty new-value)
     (let [data new-value
-          xaxis (get (aget (.getOptions @received-counts-chart) "xaxes") 0)
+          xaxis (get (aget (.getOptions @received-tps-chart) "xaxes") 0)
           [last-time _] (last new-value)
           one-minute-ago (- last-time 60000)]
       (aset xaxis "min" one-minute-ago)
       (aset xaxis "max" last-time)
-      (.setData @received-counts-chart (clj->js [data]))
-      (.setupGrid @received-counts-chart)
-      (.draw @received-counts-chart))))
+      (.setData @received-tps-chart (clj->js [data]))
+      (.setupGrid @received-tps-chart)
+      (.draw @received-tps-chart))))
 
 (defn hide-connect-button [_ _ _]
   (.hide ($ :#connect_button)))
 
 (defn render-config []
-  [[:node-create [:received-counts] prepare-received-counts-chart]
-   [:value [:received-counts] render-received-counts-chart]
+  [[:node-create [:received-tps] prepare-received-tps-chart]
+   [:value [:received-tps] render-received-tps-chart]
    
    [:transform-enable [:connected] (h/add-send-on-click "connect_button")]
    [:transform-disable [:connected] hide-connect-button]])
